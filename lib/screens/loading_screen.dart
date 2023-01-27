8c66a7d3fc1793c9d9426f2225b4b6741029fba3
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2023-01-27 15:40:37 +0700

    Initial commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'package:clima/utilities/constants.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getLocation();
+
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$API_KEY');
+
+    var weatherData = await networkHelper.getData();
+  }
+}
---
+++ lib/utilities/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+
+const API_KEY = 'c8b777d2b0e1c695c3665d24e4f3c62b';
